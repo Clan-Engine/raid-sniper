b8500e1aa7bec2fed7b0eeae6eb1c1b404d520d4
Zenthial <iamzenthial@gmail.com>
2021-04-06 00:06:37 -0400

    remove firebaseconfig from git

--- src/firebase/firebaseConfig.ts
+++
@@ -1,9 +0,0 @@
-export var raidsniperConfig = {
-    apiKey: "AIzaSyCThNlYMgBYVTn79TyYOczcrkllNes4dZk",
-    authDomain: "clan-engine-raid-sniper.firebaseapp.com",
-    databaseURL: "https://clan-engine-raid-sniper-default-rtdb.firebaseio.com",
-    projectId: "clan-engine-raid-sniper",
-    storageBucket: "clan-engine-raid-sniper.appspot.com",
-    messagingSenderId: "1087527059581",
-    appId: "1:1087527059581:web:f5ddf2a3eadaa5204ef114"
-  };
